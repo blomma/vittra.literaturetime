218c87b85a27b5e976b90e3a9a2c2928c3f49d42
Mikael Hultgren <blomma@artsoftheinsane.com>
2024-03-05 16:37:56 +0100

    Add large text preview

--- vittra.literaturetime/Preview Content/LiteratureTimePreview.swift
+++ vittra.literaturetime/Preview Content/LiteratureTimePreview.swift
@@ -12,6 +12,19 @@ extension LiteratureTime {
         )
     }
 
+    static var previewBig: LiteratureTime {
+        LiteratureTime(
+            time: "21:05",
+            quoteFirst: "It was ",
+            quoteTime: "five minutes past nine",
+            quoteLast: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris cursus mattis molestie a. Ullamcorper dignissim cras tincidunt lobortis feugiat. Faucibus nisl tincidunt eget nullam non nisi est sit. At augue eget arcu dictum varius. At tellus at urna condimentum mattis pellentesque id nibh. Feugiat nisl pretium fusce id velit ut tortor. Faucibus in ornare quam viverra orci sagittis. Orci sagittis eu volutpat odio. Volutpat est velit egestas dui id ornare arcu odio ut. Etiam dignissim diam quis enim lobortis scelerisque. Sem viverra aliquet eget sit. Amet purus gravida quis blandit turpis. Ut morbi tincidunt augue interdum velit euismod. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Sem viverra aliquet eget sit amet tellus cras. Donec ac odio tempor orci dapibus ultrices in iaculis. Id aliquet risus feugiat in ante metus. In mollis nunc sed id semper risus in. Arcu ac tortor dignissim convallis aenean. Ullamcorper sit amet risus nullam eget felis eget nunc. Quis blandit turpis cursus in hac habitasse platea dictumst. Volutpat sed cras ornare arcu dui vivamus arcu. Faucibus turpis in eu mi. In fermentum et sollicitudin ac orci phasellus egestas tellus. Egestas purus viverra accumsan in. Scelerisque varius morbi enim nunc faucibus a pellentesque sit. Risus pretium quam vulputate dignissim suspendisse in. Elit duis tristique sollicitudin nibh.",
+            title: "The Murder on the Links",
+            author: "Agatha Christie",
+            gutenbergReference: "58866",
+            id: "e42d5465bc978b08ec08d6711a0bc165d5381eaeaced8321fb33b95c85b97157"
+        )
+    }
+
     static var preview0425: LiteratureTime {
         return LiteratureTime(
             time: "04:25",
